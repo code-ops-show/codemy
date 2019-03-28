1404f305be0404ac97385805bb693e9ee2843063
Zack Siri <zacksiri@gmail.com>
2019-03-28 16:14:07 +0700

    mobile friendly membership page

--- app/pages/pages/membership/index.tsx
+++ app/pages/pages/membership/index.tsx
@@ -32,17 +32,17 @@ const Membership: FunctionComponent<RouteProps> = () => {
         <div className={hero.bg}></div>
         <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row relative container mx-auto text-white z-10'>
           <div className='flex-1 mx-5 mb-6'>
-            <h1 className='mb-10 text-center font-thin antialiased text-5xl mr-20'>{t('pages.membership.heading')}</h1>
-            <p className='leading-normal text-center font-normal text-lg mr-12'>{t('pages.membership.slogan')}</p>
+            <h1 className='mb-10 text-center font-thin antialiased text-5xl'>{t('pages.membership.heading')}</h1>
+            <p className='leading-normal text-center font-normal text-lg'>{t('pages.membership.slogan')}</p>
           </div>
         </div>
       </section>
       <section id='pricing' className={styles.pricing}>
         <div className='flex flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row relative container mx-auto text-white z-10'>
-          <div className='flex-1'>
+          <div className='flex-1 mb-10'>
             <Plan {...plans.monthly} />
           </div>
-          <div className='flex-none mt-0 sm:mt-0 md:mt-0 lg:-mt-12 xl:-mt-12'>
+          <div className='flex-1 mt-0 sm:mt-0 md:mt-0 lg:-mt-12 xl:-mt-12 mb-10'>
             <Plan {...plans.yearly} />
           </div>
           <div className='flex-1'>
@@ -50,12 +50,12 @@ const Membership: FunctionComponent<RouteProps> = () => {
           </div>
         </div>
       </section>
-      <section id='faqs' className='bg-white pt-20 pb-10'>
+      <section id='faqs' className='bg-white pt-10 pb-10'>
         <div className='container mx-auto'>
           <h2 className='text-center text-4xl font-normal'>{t('pages.membership.faqs.heading')}</h2>
           <div className='flex content-start flex-wrap items-stretch'>
             {faqs.map(faq => 
-              <div className='w-1/2'>
+              <div className='w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2'>
                 <div className='m-10'>
                   <h3 className='text-2xl font-light mb-10'>{faq.question}</h3>
                   <p className='leading-normal text-lg text-grey-dark'>
