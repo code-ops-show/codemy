b5e829788357cc42670dbe6b08422a683f0793c6
Zack Siri <zacksiri@gmail.com>
2019-03-29 22:20:26 +0700

    clean up api function

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,14 +1,16 @@
 import studio from './studio'
 import RouteNode from 'route-node'
 
-type EndpointListType = {
-  [key: string]: {
-    endpoint: any,
-    routes: RouteNode 
-  }
+type EndpointType = {
+  endpoint: any
+  routes: RouteNode
 }
 
-const endpoints: EndpointListType = { 
+type EndpointListType = {
+  [key: string]: EndpointType
+}
+
+const endpoints: EndpointListType = {
   studio: { endpoint: process.env.STUDIO, routes: studio }
 }
 
@@ -18,14 +20,9 @@ const api: Function = (
   params?: Object,
   options?: Object
 ): string => {
+  const { endpoint, routes } = endpoints[name]
 
-  const api = endpoints[name]
-  const { endpoint, routes } = api
-
-  return [
-    endpoint, 
-    routes.buildPath(path, params, options)
-  ].join('')
+  return [endpoint, routes.buildPath(path, params, options)].join('')
 }
 
 export default api
